d4e1f670bb6ae41a497966e83115389e8870673f
Bug Hunter <2185219021@qq.com>
2019-11-15 21:21:42 +0800

    191115 spring cloud - springboot

---
+++ service-eureka/src/test/java/com/bugmaker/serviceeureka/ServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugmaker.serviceeureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-user/src/test/java/com/bugmaker/serviceuser/ServiceUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugmaker.serviceuser;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
